002e3a6ac19f8caf1b1a8bf02a1241de3dc0ae00
Thyon3 <asnakemengesha80@gmail.com>
2026-01-01 02:53:18 -0500

    feat(models): add User model with JSON serialization

---
+++ lib/models/user.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  final String id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String? phone;
+  
+  User({
+    required this.id, 
+    required this.email, 
+    required this.firstName, 
+    required this.lastName, 
+    this.phone
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
