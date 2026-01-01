9d77ce2ab8314348d4465d66a70c6b190a2afe39
Thyon3 <asnakemengesha80@gmail.com>
2026-01-01 04:15:25 -0500

    fix: resolve dependency conflicts and downgrade riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' hide Provider, ChangeNotifierProvider;
 import 'package:provider/provider.dart';
 import 'package:thydelivery_mobileapp/firebase_options.dart';
 import 'package:thydelivery_mobileapp/models/restaurant.dart';
